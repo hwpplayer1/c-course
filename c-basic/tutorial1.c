d33e9bfe48eb760ae28351e9be0c3f6d60b2d4c4
Mert Gör <mertgor@masscollabs.xyz>
2023-12-21 20:14:17 +0300

    Other contributions are licensed under the terms of GPLv3+

---
+++ c-basic/tutorial1.c
@@ -0,0 +1,36 @@
+/**
+   
+# License
+
+Kaan Aslan'ın notundan faydalanılmıştır.
+
+Other contributions are licensed under the terms of GPLv3+ License like written below
+
+C, Systems Programming and UNIX/Linux course 
+
+Copyright (C) 2023 Mert Gör and contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
+
+#include<stdio.h>
+
+int main(){
+    printf("hello world\n");
+    printf("byee world\n");
+    // "\n" is a newline character
+    return 0;
+}
+
+// HAPPY CODING !!!
