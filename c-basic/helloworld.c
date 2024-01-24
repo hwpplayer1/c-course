75c4941db21b5afd4525924cba38b67125eb9aa3
Mert Gör <mertgor@masscollabs.xyz>
2024-01-24 10:47:36 +0300

    typo fix

--- c-basic/helloworld.c
+++ c-basic/helloworld.c
@@ -1,11 +1,11 @@
 /**
  * @file helloworld.c
- * @author  Mert GÖr (mertgor@masscollabs.xyz)
+ * @author  Mert Gör (mertgor@masscollabs.xyz)
  * @brief Hello World Example
  * @version 0.1
  * @date 2024-01-24
  * 
- * @copyright Copyright (c) 2024 Mert GÖr and contributors - GPLv3 or any later
+ * @copyright Copyright (c) 2024 Mert Gör and contributors - GPLv3 or any later
  * 
  */
 #include <stdio.h>
