d6d636cb70b0ce1a80700378e78226c3e4cc3494
Mert Gör <mertgor@masscollabs.xyz>
2024-01-24 20:51:11 +0300

    license update

--- c-basic/helloworld.c
+++ c-basic/helloworld.c
@@ -1,13 +1,26 @@
 /**
- * @file helloworld.c
- * @author  Mert Gör (mertgor@masscollabs.xyz)
- * @brief Hello World Example
- * @version 0.1
- * @date 2024-01-24
- * 
- * @copyright Copyright (c) 2024 Mert Gör and contributors - GPLv3 or any later
- * 
- */
+
+helloworld.c - hello world example
+   
+Copyright (C) 2023-2024 Mert Gör and contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Feel free to send an email to mertgor@masscollabs.xyz for your questions
+
+ **/
+
 #include <stdio.h>
 
 int main(){
--- c-basic/initialization-variable.c
+++ c-basic/initialization-variable.c
@@ -1,13 +1,25 @@
 /**
- * @file initialization-variable.c
- * @author Mert Gör (mertgor@masscollabs.xyz)
- * @brief Swap Algorithm with initialized variables
- * @version 0.1
- * @date 2024-01-24
- * 
- * @copyright Copyright (c) 2024 Mert Gör and contributors - GPLv3 or any later
- * 
- */
+
+initialization-variable.c - Swap Algorithm with initialized variables example
+   
+Copyright (C) 2023-2024 Mert Gör and contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Feel free to send an email to mertgor@masscollabs.xyz for your questions
+
+ **/
 
 #include <stdio.h>
 
