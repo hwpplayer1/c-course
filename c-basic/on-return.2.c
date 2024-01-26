29a23b2ccfbd61168c303526e48e70b535679241
Mert Gör <mertgor@masscollabs.xyz>
2024-01-26 08:12:14 +0300

    On Return : without return same result exists

---
+++ c-basic/on-return.2.c
@@ -0,0 +1,31 @@
+/**
+
+on-return.2.c - explaining return value : "without return same result exists"
+   
+Copyright (C) 2023-2024 Mert Gör and contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Feel free to send an email to mertgor@masscollabs.xyz for your questions
+
+ **/
+
+#include <stdio.h>
+
+int main() {
+
+  printf("I am main\n");
+
+  return 0; /* without this we'll see the same result*/
+}
