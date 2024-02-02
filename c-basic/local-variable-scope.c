fe9334f943d90a64346d77b30627ca697a42aa97
Mert Gör <mertgor@masscollabs.xyz>
2024-02-02 23:22:29 +0300

    local variable and global variable explained

---
+++ c-basic/local-variable-scope.c
@@ -0,0 +1,41 @@
+/**
+
+local-variable-scope.c - local variable and global variable explained 
+   
+Copyright (C) 2023-2024 Mert Gör and contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Feel free to send an email to mertgor@masscollabs.xyz for your questions
+
+ **/
+
+#include <stdio.h>
+
+int main() {
+  int a;
+
+  {
+    int b;
+
+    b = 20;
+    a = 10;
+    printf("a = %d, b = %d\n", a, b); /* correct */
+  }
+
+  printf("a = %d\n", a); /* correct */
+  printf("b = %d\n", b); /* wrong */
+
+  return 0;
+}
