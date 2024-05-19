4568853963e4c6f1ca6a7eb7a8b9025c98d2e074
Mert Gör <mertgor@masscollabs.xyz>
2024-05-19 22:21:16 +0300

    deleted old codes We will hack code again

--- c-basic/foo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int foo(){
-    printf("I am foo\n");
-}
-
-int main(){
-    foo();
-}
--- c-basic/return.2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("I am main\n");
-
-    return 0; // with our without same result will be executed
-}
