de6254a8495083d84750ea64da3e1ade41526492
Mert Gör <mertgor@masscollabs.xyz>
2024-06-30 15:20:16 +0300

    Scanf two numbers

---
+++ c-basic/scanf_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+
+    printf("enter two numers a and b:");
+    scanf("%d%d", &a, &b);
+    printf("a = %d, b = %d\n", a, b);
+
+    return 0;
+}
