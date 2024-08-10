8d31c9aa48c457a08019e4ecddbbd55affdd378d
Mert Gör <mertgor@masscollabs.xyz>
2024-08-10 16:25:34 +0300

    scanf update

---
+++ c-basic/klavyeden_okuma_scanf_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int a;
+  int b;
+
+  printf("enter number a :");
+  scanf("%d", &a);
+  printf("a is %d\n", a);
+  printf("enter number b :");
+  scanf("%d", &b);
+  printf("b is %d\n",b);
+  
+  return 0;
+}
