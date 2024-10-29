3f8ba5ba8050c64cfb83b42a2e2a63f02c09475f
Mert Gör <mertgor@masscollabs.xyz>
2024-10-29 21:59:59 +0300

    update

---
+++ c-basic/abc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  double a,b,c;
+
+  printf("enter a number \n");
+  scanf("%lf", &a);
+
+  printf("enter b number \n");
+  scanf("%lf", &b);
+
+  c = a + b;
+  
+  printf("your result and also c number is : %f\n", c);
+
+  return 0;
+}
