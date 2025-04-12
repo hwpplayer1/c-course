79f83aa19044ec950e238d7b547949df4c02e7ae
Mert Gör <mertgor@masscollabs.xyz>
2025-04-12 22:09:27 +0300

    int main() {     unsigned int a; // Change 'int' to 'unsigned int'
        printf("Enter your number: ");
        scanf("%x", &a); // Now 'a' matches '%x' expected type
        printf("a = %u\n", a); // Use '%u' to correctly print an unsigned int
    
        return 0;
    }

--- c-basic/hex_read_scanf.c
+++ c-basic/hex_read_scanf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main()
-{
-  int a, b;
+int main() {
+    unsigned int a; // Change 'int' to 'unsigned int'
 
-  printf("Enter your number : ");
-  scanf("%x", &a);
-  printf("a = %d\n", a);
+    printf("Enter your number: ");
+    scanf("%x", &a); // Now 'a' matches '%x' expected type
+    printf("a = %u\n", a); // Use '%u' to correctly print an unsigned int
 
-  return 0;
+    return 0;
 }
